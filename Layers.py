22a8a9f7003a37eaf9c581968916d2aabad4456c
demdecuong <nguyenphucminh2804@gmail.com>
2020-08-02 18:10:00 +0700

    refactor Sublayers

--- Layers.py
+++ Layers.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from SubLayers import point_wise_feed_forward_network,MultiHeadAttention
+from Sublayers import point_wise_feed_forward_network,MultiHeadAttention
 
 class EncoderLayer(tf.keras.layers.Layer):
     def __init__(self, d_model, num_heads, dff, rate=0.1):
@@ -62,32 +62,6 @@ class DecoderLayer(tf.keras.layers.Layer):
         out3 = self.layernorm3(ffn_output + out2)  # (batch_size, target_seq_len, d_model)
         
         return out3, attn_weights_block1, attn_weights_block2
-
-class EncoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, dff, rate=0.1):
-        super(EncoderLayer, self).__init__()
-
-        self.mha = MultiHeadAttention(d_model, num_heads)
-        self.ffn = point_wise_feed_forward_network(d_model, dff)
-
-        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        
-        self.dropout1 = tf.keras.layers.Dropout(rate)
-        self.dropout2 = tf.keras.layers.Dropout(rate)
-    
-    def call(self, x, training, mask):
-
-        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
-        
-        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
-        
-        return out2
-
 
 
 if __name__ == '__main__':
